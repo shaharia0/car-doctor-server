f2a184f9c0803bd6703513fde1ed649c078538f0
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-22 00:38:42 +0600

    added update data on server site

--- index.js
+++ index.js
@@ -58,14 +58,38 @@ async function run() {
       console.log(query);
       const result = await bookingCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
     app.post('/bookings', async(req, res)=> {
       const booking = req.body;
       console.log(booking);
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
+    });
+    
+    app.patch('/bookings/:id', async(req, res)=> {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+       const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+       };
+       const result = await bookingCollection.updateOne(filter, updateDoc);
+       res.send(result);
+      
     })
+  
+
+    app.delete('/bookings/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
