10bab0576caa448bbb5c71e185b8c44de27594ae
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-23 21:27:23 +0600

    added jwt local storage data in server side

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+    console.log('hitting verify JWT');
+    console.log(req.headers.authorization);
+    const authorization = req.headers.authorization;
+    if(!authorization){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    const token = authorization.split(' ')[1];
+    console.log('token inside verify JWT', token);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+      if(error){
+        return res.status(403).send({error: true, message: 'unauthorized access'})
+      }
+      req.decoded = decoded;
+      next();
+    })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -58,9 +76,9 @@ async function run() {
       res.send(result);
     })
 
-    // bookings database code
-    app.get('/bookings', async(req, res)=> {
-      console.log(req.query.customerEmail);
+    // bookings routes database code 
+    app.get('/bookings', verifyJWT, async(req, res)=> {
+      // console.log(req.headers.authorization);
       let query = {};
       if(req.query?.customerEmail){
         query = {customerEmail: req.query.customerEmail}
