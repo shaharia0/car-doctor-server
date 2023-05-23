21f239c1255adee581cfef6630a49719e86c760a
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-23 20:33:06 +0600

    added jwt local storage data

--- index.js
+++ index.js
@@ -32,6 +32,15 @@ async function run() {
     const serviceCollection = client.db('carDoctor').collection('services');
     const bookingCollection = client.db('carDoctor').collection('bookings');
 
+    // jwt 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '10h'});
+      res.send({token});
+    })
+    // services routes
     app.get('/services', async(req, res)=>{
          const cursor = serviceCollection.find();
          const result = await cursor.toArray();
